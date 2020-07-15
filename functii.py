6a60b9ba2cfc2f8a4774ea781ca67cd720c580bf
rangademetal <60630480+rangademetal@users.noreply.github.com>
2020-07-15 09:11:10 -0700

    function

---
+++ functii.py
@@ -0,0 +1,46 @@
+number1 = 7
+number2 = 5
+# if number1 < number2:
+#     print("Da")
+# else:
+#     print("Nu")
+#
+#
+#
+# print('DA' if number1 > number2 else 'NO')
+#
+#
+# vard = True if number1 < number2 else False
+# print(vard)
+#
+# numbar ='sdasdas'
+# print(numbar.isdigit())
+#
+#
+# i = 9
+# j = 1
+# while j != i:
+#     # j += 1
+#     j = j+1
+#     print(j)
+
+number2 = 2
+number1 = 2
+
+if number1 == number2:
+    print("ok")
+
+items = 10
+str = 'sdasdadas'
+dasda = 0
+print("For")
+# for dasda in str:
+#     print(dasda, end="")
+i = 0
+print("While")
+while i != int(items):
+    i = i +1
+    print(i)
+    if(i == items):
+        print("Break")
+        break
