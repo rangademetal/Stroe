3c7f5436aea495325c544c15e7a62eafe83d200d
JohnForte <inq7@hotmail.com>
2020-07-15 17:36:16 +0300

    1st

---
+++ da.py
@@ -0,0 +1,40 @@
+
+text = "123"
+text = int(text)
+no = 1
+sum = text+no
+
+# test to switch str to int
+
+print(sum)
+
+no1 = 1.2
+
+sum = float(sum)
+sum1 = sum+no1
+
+print(sum1)
+
+# test to switch int to float and add a float
+
+intvalue = input()
+
+intvalue = int(intvalue)
+sum3= intvalue+1
+
+print(sum3)
+
+# test to take a int input and add it to a standalone no
+
+int5= 10
+int6 = "11.5"
+
+int5=float(int5)
+int6=float(int6)
+
+sum3=int5+int6
+
+print(sum3)
+
+# test to convert values to a float
+
